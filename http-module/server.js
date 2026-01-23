685f449481e60a93295b9297cfc9ff9f0641b22e
Ajmat-Ali <badshah9631619358@gmail.com>
2026-01-23 23:57:58 +0530

    learn: query params in node.js and extract by built-in global class URL

--- http-module/server.js
+++ http-module/server.js
@@ -1,9 +1,32 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   console.log("URL :", req.url);
+//   const url = req.url.split("?")[1].split("&");
+//   let urlObj = {};
+//   for (let key in url) {
+//     let [uKey, uVal] = url[key].split("=");
+//     urlObj[uKey] = uVal;
+//   }
+//   console.log("Url 2--->", url);
+//   console.log("Url 3--->", urlObj);
+
+//   res.end("You have successfully build your first server");
+// });
+// console.log("Hi......");
+
+// server.listen(3000, () => {
+//   console.log("server running on  http://localhost:3000");
+// });
+
+// --------------------------------------- Query Params extracting with built-in node class (Global Class)  ---------------------
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("URL ---> ", req.url);
-  console.log("Method ---> ", req.method);
-  console.log(req);
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  console.log(url.pathname);
+  console.log(url.searchParams.get("category"));
+  console.log(url.searchParams.get("price"));
 
   res.end("You have successfully build your first server");
 });
